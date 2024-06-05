c10b5221638b97d53187aab9ce3065a202d1233d
zhouzhipeng <admin@zhouzhipeng.com>
2024-06-05 19:52:26 +0400

    impl serialize& deserialbel for GD<T>

--- godot-core/src/builtin/custom/mod.rs
+++ godot-core/src/builtin/custom/mod.rs
@@ -1,9 +1,15 @@
+use std::fmt;
 use std::fmt::Display;
-use serde::Serialize;
+use std::marker::PhantomData;
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+use serde::de::value::I64Deserializer;
+use serde::de::{Error, Visitor};
 use crate::builtin::{Dictionary, GString, Variant};
 use crate::builtin::meta::{ConvertError, FromGodot, GodotConvert, ToGodot};
-use crate::engine::Json;
+use crate::engine::{Area2D, IObject, Json, Object, StaticBody2D, Timer};
+use crate::global::instance_from_id;
 use crate::godot_print;
+use crate::obj::{Gd, GodotClass, Inherits};
 
 type Anyhow<T> = anyhow::Result<T>;
 
@@ -46,3 +52,41 @@ impl<T> FromGodot for Anyhow<T> {
         Err(ConvertError::new("not supported"))
     }
 }
+
+macro_rules! impl_serialize_deserialize {
+    ($t: ident) => {
+
+impl Serialize for Gd<$t>{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        serializer.serialize_str(&self.instance_id().to_string())
+    }
+}
+
+
+impl<'de> Deserialize<'de> for Gd<$t> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        struct MyStructVisitor;
+
+        impl<'de> Visitor<'de> for MyStructVisitor{
+            type Value = Gd<$t>;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("an i64")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: Error {
+                let value = v.parse::<i64>().unwrap();
+                Ok(instance_from_id(value).unwrap().cast::<$t>())
+            }
+        }
+
+        deserializer.deserialize_str(MyStructVisitor)
+    }
+}
+    };
+}
+
+
+impl_serialize_deserialize!(Area2D);
+impl_serialize_deserialize!(StaticBody2D);
+impl_serialize_deserialize!(Timer);
