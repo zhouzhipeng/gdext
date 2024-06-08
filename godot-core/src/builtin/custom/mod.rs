9aea5f1a28d13a5b646ab9f5bed07c22d011a6f2
zhouzhipeng <admin@zhouzhipeng.com>
2024-06-08 08:47:36 +0400

    comment deserialize for GD<T> ,or it will make memory leak

--- godot-core/src/builtin/custom/mod.rs
+++ godot-core/src/builtin/custom/mod.rs
@@ -55,59 +55,72 @@ impl<T> FromGodot for Anyhow<T> {
     }
 }
 
-macro_rules! impl_serialize_deserialize {
-    ($t: ident) => {
-
-impl Serialize for Gd<$t>{
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-        serializer.serialize_str(&self.instance_id().to_string())
-    }
-}
-
-
-impl<'de> Deserialize<'de> for Gd<$t> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
-        struct MyStructVisitor;
-
-        impl<'de> Visitor<'de> for MyStructVisitor{
-            type Value = Gd<$t>;
-
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("an i64")
-            }
-
-            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: Error {
-                use serde::de::{self, Visitor, Unexpected};
-
-                let value = v.parse::<i64>().unwrap();
-                if is_instance_id_valid(value){
-                    let r = instance_from_id(value).unwrap().try_cast::<$t>();
-                    match r{
-                        Ok(s)=>{
-                            return Ok(s);
-                        }
-                        Err(e)=>{
-                            return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
-                        }
-                    }
-                }else{
-                   return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
-                }
-
-                //      let value = v.parse::<i64>().unwrap();
-                // Ok(instance_from_id(value).unwrap().cast::<$t>())
-
-            }
-        }
-
-        deserializer.deserialize_str(MyStructVisitor)
-    }
-}
-    };
-}
-
-
-impl_serialize_deserialize!(Area2D);
-impl_serialize_deserialize!(StaticBody2D);
-impl_serialize_deserialize!(Timer);
-impl_serialize_deserialize!(Texture2D);
+// fix: deserialize is not safe here
+//
+// macro_rules! impl_serialize_deserialize {
+//     ($t: ident) => {
+//
+// impl Serialize for Gd<$t>{
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+//         serializer.serialize_str(&format!("INST{}",self.instance_id().to_i64()))
+//     }
+// }
+//
+//
+// impl<'de> Deserialize<'de> for Gd<$t> {
+//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+//         struct MyStructVisitor;
+//
+//         impl<'de> Visitor<'de> for MyStructVisitor{
+//             type Value = Gd<$t>;
+//
+//             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+//                 formatter.write_str("an i64")
+//             }
+//
+//             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: Error {
+//                 use serde::de::{self, Visitor, Unexpected};
+//                 //
+//
+//                 if v.starts_with("INST"){
+//                     let value = v[4..].parse::<i64>().unwrap();
+//                     if is_instance_id_valid(value){
+//                         use crate::sys;
+//                         use crate::meta::PtrcallSignatureTuple;
+//                         let instance_id = value;
+//
+//                         let r = instance_from_id(value).unwrap();//.try_cast::<$t>();
+//                         drop(r);
+//                         // match r{
+//                         //     Ok(s)=>{
+//                         //         return Ok(s);
+//                         //     }
+//                         //     Err(e)=>{
+//                                 return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
+//                         //     }
+//                         // }
+//                         // use crate::obj::NewAlloc;
+//                         // return Ok($t::new_alloc())
+//                     }else{
+//                        return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
+//                     }
+//                 }
+// return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
+//
+//                 //      let value = v.parse::<i64>().unwrap();
+//                 // Ok(instance_from_id(value).unwrap().cast::<$t>())
+//
+//             }
+//         }
+//
+//         deserializer.deserialize_str(MyStructVisitor)
+//     }
+// }
+//     };
+// }
+//
+//
+// impl_serialize_deserialize!(Area2D);
+// impl_serialize_deserialize!(StaticBody2D);
+// impl_serialize_deserialize!(Timer);
+// impl_serialize_deserialize!(Texture2D);
