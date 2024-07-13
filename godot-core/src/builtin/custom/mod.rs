580d24aaada804504705613a173c28df17db42e1
ronnie <ronnie@binance.com>
2024-07-13 10:03:56 +0800

    support Vec<CustomEnum> for @export

--- godot-core/src/builtin/custom/mod.rs
+++ godot-core/src/builtin/custom/mod.rs
@@ -5,13 +5,14 @@ use anyhow::anyhow;
 use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use serde::de::value::I64Deserializer;
 use serde::de::{Error, Visitor};
-use crate::builtin::{Dictionary, GString, Variant};
+use crate::builtin::{Array, Dictionary, GString, Variant};
 use crate::builtin::meta::{ConvertError, FromGodot, GodotConvert, ToGodot};
 use crate::classes::Texture2D;
 use crate::engine::{Area2D, IObject, Json, Object, StaticBody2D, Timer};
-use crate::global::{instance_from_id, is_instance_id_valid};
+use crate::global::{instance_from_id, is_instance_id_valid, PropertyHint};
 use crate::godot_print;
 use crate::obj::{Gd, GodotClass, Inherits};
+use crate::registry::property::{Export, PropertyHintInfo, Var};
 
 type Anyhow<T> = anyhow::Result<T>;
 
@@ -55,72 +56,53 @@ impl<T> FromGodot for Anyhow<T> {
     }
 }
 
-// fix: deserialize is not safe here
-//
-// macro_rules! impl_serialize_deserialize {
-//     ($t: ident) => {
-//
-// impl Serialize for Gd<$t>{
-//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-//         serializer.serialize_str(&format!("INST{}",self.instance_id().to_i64()))
-//     }
-// }
-//
-//
-// impl<'de> Deserialize<'de> for Gd<$t> {
-//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
-//         struct MyStructVisitor;
-//
-//         impl<'de> Visitor<'de> for MyStructVisitor{
-//             type Value = Gd<$t>;
-//
-//             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-//                 formatter.write_str("an i64")
-//             }
-//
-//             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where E: Error {
-//                 use serde::de::{self, Visitor, Unexpected};
-//                 //
-//
-//                 if v.starts_with("INST"){
-//                     let value = v[4..].parse::<i64>().unwrap();
-//                     if is_instance_id_valid(value){
-//                         use crate::sys;
-//                         use crate::meta::PtrcallSignatureTuple;
-//                         let instance_id = value;
-//
-//                         let r = instance_from_id(value).unwrap();//.try_cast::<$t>();
-//                         drop(r);
-//                         // match r{
-//                         //     Ok(s)=>{
-//                         //         return Ok(s);
-//                         //     }
-//                         //     Err(e)=>{
-//                                 return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
-//                         //     }
-//                         // }
-//                         // use crate::obj::NewAlloc;
-//                         // return Ok($t::new_alloc())
-//                     }else{
-//                        return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
-//                     }
-//                 }
-// return Err(de::Error::invalid_value(Unexpected::Str(v), &self));
-//
-//                 //      let value = v.parse::<i64>().unwrap();
-//                 // Ok(instance_from_id(value).unwrap().cast::<$t>())
-//
-//             }
-//         }
-//
-//         deserializer.deserialize_str(MyStructVisitor)
-//     }
-// }
-//     };
-// }
-//
-//
-// impl_serialize_deserialize!(Area2D);
-// impl_serialize_deserialize!(StaticBody2D);
-// impl_serialize_deserialize!(Timer);
-// impl_serialize_deserialize!(Texture2D);
+
+impl<T:GodotConvert<Via =i32> + ToGodot + FromGodot + Var> GodotConvert for Vec<T>
+{
+    type Via = Array<i32>;
+}
+
+impl<T:GodotConvert<Via =i32> + ToGodot + FromGodot + Var> ToGodot for Vec<T>{
+    fn to_godot(&self) -> Self::Via {
+        let mut array = Array::new();
+        for x in self {
+            array.push(x.to_godot());
+        }
+        array
+    }
+}
+
+impl<T:GodotConvert<Via =i32> + ToGodot + FromGodot + Var> FromGodot for  Vec<T>{
+    fn try_from_godot(via: Self::Via) -> Result<Self, ConvertError> {
+        let mut ret = vec![];
+        for x in via.iter_shared() {
+            ret.push(T::from_godot(x));
+        }
+        Ok(ret)
+    }
+}
+
+impl<T:GodotConvert<Via =i32> + ToGodot + FromGodot + Var> Var for  Vec<T>
+{
+    fn get_property(&self) -> Self::Via {
+        ToGodot::to_godot(self)
+    }
+
+    fn set_property(&mut self, value: Self::Via) {
+        *self = FromGodot::from_godot(value);
+    }
+    fn property_hint() -> PropertyHintInfo {
+
+        PropertyHintInfo{
+            hint: PropertyHint::ARRAY_TYPE,
+            // "hint_string": str(TYPE_INT) + "/" + str(PROPERTY_HINT_ENUM) + ":" + ",".join(CustomEnum.keys())
+            hint_string: format!("2/2:{}",  T::property_hint().hint_string.to_string()).into(),
+        }
+    }
+}
+
+impl<T:GodotConvert<Via =i32> + ToGodot + FromGodot + Var> Export for Vec<T>{
+    fn default_export_info() -> PropertyHintInfo {
+        Self::property_hint()
+    }
+}
